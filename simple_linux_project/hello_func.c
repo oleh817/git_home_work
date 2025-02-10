b4e47dc5ea6814c0ee9d95b9d55cc1c3c65d69ac
Oleh <oleh.romanko@ringteam.com>
2025-02-10 23:22:53 +0200

    Stop tracking .DS_Store file

---
+++ simple_linux_project/hello_func.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hello_make.h"
+
+/* Function definition */
+void printfHelloMake(void) {
+    printf("Hello from hello_func.c file!\n");
+    return;
+}
---
+++ simple_linux_project/hello_make.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_MAKE_H
+#define HELLO_MAKE_H
+
+/* Function declaration */
+void printfHelloMake(void);
+
+#endif
